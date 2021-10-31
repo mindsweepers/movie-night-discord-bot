ebd4861501a16fe7aceb9a0d60eb3d84f68d9c01
ABOTlegacy <abotlegacy@outlook.com>
2021-10-30 23:01:33 -0500

    added a list command so that you can read the saved movies.

--- src/commands/movie-night/list.js
+++ src/commands/movie-night/list.js
@@ -1,3 +1,5 @@
+const Discord = require('discord.js')
+const { MessageEmbed } = require('discord.js');
 const { Command } = require('@sapphire/framework');
 const movieSchema = require('../../lib/schemas/movie-schema.js');
 
@@ -5,7 +7,7 @@ module.exports = class BotCommand extends Command {
 	constructor(context) {
 		super(context, {
 			name: 'list',
-			aliases: [''],
+			aliases: ['list'],
 			description: 'List all the current movie submissions.',
 			preconditions:[{ name: 'Cooldown', context: {
 				delay: 30000,
@@ -13,10 +15,26 @@ module.exports = class BotCommand extends Command {
 		});
 	}
 
-
 	async messageRun(message, args) {
+		var str = "";
+		var filter = { };
+
+		// Fetch
+		const movies = await movieSchema.find(filter);
+		movies.forEach(movie => {
+			str += movie.title + "\n";
+		});
 		
-
-
+		// Display The List
+		const embed = new MessageEmbed()
+		.setTitle('List Of Movie Submissions')
+		.addFields(
+			{
+				name: 'Sorted by Queue Order',
+				value: str,
+				inline: true
+			}
+		)
+		message.channel.send({ embeds: [embed] });
 	}
 };
